9799d3d84be422edfc51529e72636e99ae7cc9ee
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2023-06-27 20:59:08 -0300

    criação de temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { ThemeProvider } from 'styled-components';
 import { Button } from "./componets/Button";
+import { defaultTheme } from './styles/themes/default';
 
 export function App() {
 
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant='primary' />
       <Button variant='secundary' />
       <Button variant='danger' />
       <Button variant='success' />
       <Button  />
-    </>
+    </ThemeProvider>
   )
 }
