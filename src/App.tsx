63690bed2f1d56ac250e60c768fa20e224537bc9
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2023-06-27 21:37:08 -0300

    estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { ThemeProvider } from 'styled-components';
 import { Button } from "./componets/Button";
 import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
+
       <Button variant='primary' />
       <Button variant='secundary' />
       <Button variant='danger' />
       <Button variant='success' />
       <Button  />
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
