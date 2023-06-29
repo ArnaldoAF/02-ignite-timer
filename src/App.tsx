30f0b9fc85ca7586167f2312607675862bf61ac5
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2023-06-29 17:00:15 -0300

    rout layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
+import { Header } from './components/Header'
 
 export function App() {
     return (
