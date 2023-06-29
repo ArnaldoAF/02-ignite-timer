b7cae13510514571e73edd6a7055a65617c571c4
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2023-06-29 15:31:37 -0300

    es-lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import { ThemeProvider } from 'styled-components';
-import { Button } from "./componets/Button";
-import { defaultTheme } from './styles/themes/default';
-import { GlobalStyle } from './styles/global';
+import { ThemeProvider } from 'styled-components'
+import { Button } from './componets/Button'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
-
-      <Button variant='primary' />
-      <Button variant='secundary' />
-      <Button variant='danger' />
-      <Button variant='success' />
-      <Button  />
+      <Button variant="primary" />
+      <Button variant="secundary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <Button />
 
       <GlobalStyle />
     </ThemeProvider>
