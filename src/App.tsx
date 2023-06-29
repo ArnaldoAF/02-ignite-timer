f1b969c60a886b5fce99a7e8ee3c76369fa61779
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2023-06-29 16:36:27 -0300

    react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './componets/Button'
+
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secundary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-      <Button />
-
-      <GlobalStyle />
-    </ThemeProvider>
-  )
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <BrowserRouter>
+                <Router />
+            </BrowserRouter>
+            <GlobalStyle />
+        </ThemeProvider>
+    )
 }
