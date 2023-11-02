83d94626003dd47ae8ad4fd35cbf4b01587d5eb5
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2023-11-02 18:33:56 -0300

    Contexto no formulário

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -1,12 +1,16 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect } from 'react'
 import { CountdownContainer, Separator } from './styles'
 import { differenceInSeconds } from 'date-fns'
 import { CyclesContext } from '../..'
 
 export function Countdown() {
-    const { activeCycle, activeCycleId, markCurrentCycleAsFinished } =
-        useContext(CyclesContext)
-    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+    const {
+        activeCycle,
+        activeCycleId,
+        markCurrentCycleAsFinished,
+        amountSecondsPassed,
+        setSecondsPassed,
+    } = useContext(CyclesContext)
 
     const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
     const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
@@ -30,11 +34,11 @@ export function Countdown() {
                 if (secondsDifference >= totalSeconds) {
                     markCurrentCycleAsFinished()
 
-                    setAmountSecondsPassed(totalSeconds)
+                    setSecondsPassed(totalSeconds)
                     clearInterval(interval)
                     // setActiveCycleId(null)
                 } else {
-                    setAmountSecondsPassed(secondsDifference)
+                    setSecondsPassed(secondsDifference)
                 }
             }, 1000)
         }
@@ -42,7 +46,13 @@ export function Countdown() {
         return () => {
             clearInterval(interval)
         }
-    }, [activeCycle, totalSeconds, activeCycleId, markCurrentCycleAsFinished])
+    }, [
+        activeCycle,
+        totalSeconds,
+        activeCycleId,
+        markCurrentCycleAsFinished,
+        setSecondsPassed,
+    ])
 
     useEffect(() => {
         if (activeCycle) document.title = `${minutes}:${seconds}`
--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,31 +1,17 @@
 import { MinutesAmountInput, TaskInput, FormContainer } from './styles'
-import * as zod from 'zod'
-import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-
-const newCycleFormValidationSchema = zod.object({
-    task: zod.string().min(1, 'Informe a tarefa'),
-    minutesAmount: zod
-        .number()
-        .min(1, 'O intervalo precisa ser de no mínimo 5 minutos')
-        .max(60, 'O intervalo precisa ser de no máximo 60 minutos'),
-})
+import { useFormContext } from 'react-hook-form'
+import { useContext } from 'react'
+import { CyclesContext } from '../..'
 
 // interface NewCycleFormData {
 //     task: string
 //     minutesAmount: number
 // }
 
-type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
-
 export function NewCycleForm() {
-    const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
-        resolver: zodResolver(newCycleFormValidationSchema),
-        defaultValues: {
-            task: '',
-            minutesAmount: 0,
-        },
-    })
+    const { activeCycle } = useContext(CyclesContext)
+    const { register } = useFormContext()
+
     return (
         <FormContainer>
             <label htmlFor="task">Vou trabalhar em</label>
@@ -50,7 +36,7 @@ export function NewCycleForm() {
                 id="minutesAmount"
                 placeholder="00"
                 step={5}
-                min={1}
+                min={5}
                 max={60}
                 disabled={!!activeCycle}
                 {...register('minutesAmount', { valueAsNumber: true })}
